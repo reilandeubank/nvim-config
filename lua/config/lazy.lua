5b2c3c7a5a512f0f0fa1eb4f9f4bea6e6d5676e3
Reiland Eubank <reiland.eubank@garmin.com>
2026-01-27 10:57:03 -0600

    fix: reorder plugin imports to fix LazyVim warning
    - Move python extras before custom plugins import
    - Fixes 'lazyvim.plugins should be first, followed by extras, then custom plugins' warning
    - Removes conditional filetype imports that were causing ordering issues
    
    Change-Id: Icd3741eb52ab4a63e49e5a3361a64f9f09177d7e

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,13 +13,10 @@ require("lazy").setup({
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
-    -- Enable plugins for specific file types
-    { filetype = "python", import = "lazyvim.plugins.extras.lang.python" },
-    -- { filetype = "javascript", import = "lazyvim.plugins.extras.lang.javascript" },
-    -- { filetype = "jsx", import = "lazyvim.plugins.extras.lang.jsx" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
