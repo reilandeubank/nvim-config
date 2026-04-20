eebd4f31994b5a9ad49b3d2e5874d97883dd9a4d
Reiland Eubank <reiland.eubank@garmin.com>
2026-04-20 08:35:35 -0500

    Fix clipboard
    Change-Id: I03ce63fae3761f5a2528c4b579af0e0ab81a50f5

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.opt.clipboard = "unnamedplus"
+
 -- Use OSC 52 for clipboard so yanking/pasting works over SSH (e.g. Windows Terminal)
 vim.g.clipboard = {
   name = "OSC 52",
